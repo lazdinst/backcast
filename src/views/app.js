ab79b8c99e129117375747cc7319d0a96a957daf
Nauda <Nauda@MAcBooks-MacBook-Air.local>
2017-07-18 09:00:13 -0700

    Added Pseudo code

--- src/views/app.js
+++ src/views/app.js
@@ -8,14 +8,32 @@ var AppView = Backbone.View.extend({
     
   },
 
-
   render: function() {
+    //Render App View
+    //AppView is created on the index.html when backbone initializes
+    //Sample data is 'imported' in the AppView.initialize function above
     this.$el.html(this.template());
-    var videoPlayerView = new VideoPlayerView({collection: this.videos});
-    var videoListView = new VideoListView({collection: this.videos});
+
+    //Render Video Player View
+    //What am I passing into VideoPlayerView? => The Selected Video?
+    var videoPlayerView = new VideoPlayerView( /*- What am I passing? Object of Attributes? -*/);
+    this.$el.html(this.template(this.model.attributes));
+
+
+    //Render VideoList View => Am I passing this.videos? It should be the collection of example data
+    var videoListView = new VideoListView( /*- What am I passing? Object of Attributes? -*/);
+    this.$el.html(this.template(this.model.attributes));
+
+    //Render VideoList Entry View => this should grab the first 5 fives from the VideoListView?
+    var videoListEntryView = new VideoListEntryView( /*- What am I passing? Object of Attributes? -*/);
+    this.$el.html(this.template(this.model.attributes));
+
+    //Render Search View
+    this.$el.html(this.template());
+    var searchView = new SearchView( /*- What am I passing? Object of Attributes? -*/ );
+
     return this;
   },
 
   template: templateURL('src/templates/app.html')
-
 });
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,39 +1,42 @@
 var VideoListView = Backbone.View.extend({
   
   initialize: function() {
+    //This Initialized tag is janky, need to find more appropriate solution
     this.initialized = false;
     this.render();
     this.collection.on('sync', this.render, this);
     this.initialized = true;
   },
 
-  renderVideo: function(video) {
-    var i = 0;
-    while (i < 5) {
-      var videoListEntryView = new VideoListEntryView({model: video[i]});
-      this.$el.append(videoListEntryView.render());
-      i++;
+  renderVideo: function(videos) {
+    for(var i = 0; i < 5; i ++) {
+      var videoListEntryView = new VideoListEntryView({model: videos[i]});
+      this.$el.append(videoListEntryView.render());      
     }
   },
   
-  //Original Below
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+
+    //If not initialized, render videos for the first time.
     if (this.initialized) {
       this.renderVideo(this.collection.models);
     }
+
+    
     return this;
   },
 
   //Original Below
   template: templateURL('src/templates/videoList.html')
-  
-
 });
 
-
-
-    // for (var i = 0; i < 5; i++) {
-    //   this.renderMovie(this.collection.models[i]);
+   
+   //OLD CODE - While loop to generate the 5 video in the list entry view 
+    // var i = 0;
+    // while (i < 5) {
+    //   var videoListEntryView = new VideoListEntryView({model: videos[i]});
+    //   this.$el.append(videoListEntryView.render());
+    //   i++;
     // }
